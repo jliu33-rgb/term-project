d2459c266c660231db24ac6cdd5bec284d6c02e9
jliu33-rgb <jliu33@scu.edu>
2026-03-02 18:45:32 -0800

    Add header comments to dataset.c
    Added header comments for dataset.c file.

--- app1/dataset.c
+++ app1/dataset.c
@@ -1,1 +1,11 @@
+/*
+ * File:   dataset.c
+ * Author: Jiaming Liu
+ * Desc:   DATASET ADT for Application 1.
+ *         Implementation uses a fixed-size array sorted by age.
+ */
 
+#include <assert.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "dataset.h"
