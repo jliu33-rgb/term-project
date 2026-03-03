5d0fcbbb297b4f6fd2e3856fc88a181b2a54f234
jliu33-rgb <jliu33@scu.edu>
2026-03-02 18:41:23 -0800

    write header file for app1 dataset
    Used styles consistent with previous labs

--- app1/dataset.h
+++ app1/dataset.h
@@ -1,1 +1,23 @@
+/*
+ * File:        dataset.h
+ * Description: Header file
+ * Author: Jeremy Liu
+ * Date: 3/2/26
+ */
 
+# ifndef DATASET_H
+# define DATASET_H
+
+# include <stdbool.h>
+
+typedef struct dataset DATASET;
+
+DATASET *createDataSet(int maxStudents);
+void destroyDataSet(DATASET *dp);
+
+bool insertion(DATASET *dp, int id, int age);
+int searchAge(DATASET *dp, int age);
+int deletion(DATASET *dp, int age);
+int maxAgeGap(DATASET *dp);
+
+# endif /* DATASET_H */
