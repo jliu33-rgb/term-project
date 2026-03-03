c4cdeebca8720be399aac4d98fedd51b2d7dc77c
jliu33-rgb <jliu33@scu.edu>
2026-03-02 21:17:56 -0800

    Create dataset.h for dataset management
    header file for application 2 dataset

--- app2/dataset.h
+++ app2/dataset.h
@@ -1,1 +1,21 @@
+/*
+ * File:        dataset.h
+ * Description: Headerfile for app2
+ * Author: Jiaming Liu
+ * Date: 3/2/26
+ */
 
+# ifndef DATASET_H
+# define DATASET_H
+# include <stdbool.h>
+
+typedef struct dataset DATASET;
+
+DATASET *createDataSet(int maxStudents);
+void destroyDataSet(DATASET *dp);
+
+bool insertion(DATASET *dp, int id, int age);
+bool searchID(DATASET *dp, int id);
+bool deletion(DATASET *dp, int id);
+
+# endif /* DATASET_H */
