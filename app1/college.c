b3fab6da013752061111053ad7f7c41c948702ab
jliu33-rgb <jliu33@scu.edu>
2026-03-02 20:15:43 -0800

    Add header comments to college.c
    Added header comments to college.c for documentation.

--- app1/college.c
+++ app1/college.c
@@ -1,1 +1,11 @@
+/*
+ * File:   college.c
+ * Author: Jiaming Liu
+ * Desc:   Driver program for Application 1.
+ * Date: 3/2/26
+ */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "dataset.h"
