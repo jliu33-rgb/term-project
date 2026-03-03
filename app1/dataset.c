23f7bee0a60ffe702cc133a86d6eb899e42b7d2c
jliu33-rgb <jliu33@scu.edu>
2026-03-02 18:51:24 -0800

    add function skeletons app 1 dataset.c

--- app1/dataset.c
+++ app1/dataset.c
@@ -9,3 +9,43 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include "dataset.h"
+
+typedef struct {
+    int id;
+    int age;
+} STUDENT;
+
+struct dataset {
+    int count;
+    int length;
+    STUDENT *elts;
+};
+
+DATASET *createDataSet(int maxStudents)
+{
+    return NULL;
+}
+
+void destroyDataSet(DATASET *dp)
+{
+}
+
+bool insertion(DATASET *dp, int id, int age)
+{
+    return false;
+}
+
+int searchAge(DATASET *dp, int age)
+{
+    return 0;
+}
+
+int deletion(DATASET *dp, int age)
+{
+    return 0;
+}
+
+int maxAgeGap(DATASET *dp)
+{
+    return 0;
+}
