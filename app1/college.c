8d94030c258f8b184d8430abd45d64db4b1d4bf4
jliu33-rgb <jliu33@scu.edu>
2026-03-02 20:53:29 -0800

    Add random age generation and dataset operations
    finish main file with everything

--- app1/college.c
+++ app1/college.c
@@ -9,3 +9,38 @@
 #include <stdlib.h>
 #include <time.h>
 #include "dataset.h"
+
+static int randOneOrTwo(void)
+{
+    return (rand() % 2) + 1;
+}
+
+static int randInRange(int lo, int hi)
+{
+    return lo + (rand() % (hi - lo + 1));
+}
+
+int main(void)
+{
+    srand((unsigned)time(NULL));
+
+    DATASET *dp = createDataSet(3000);
+
+    int id = randOneOrTwo();
+    for (int i = 0; i < 1000; i++) {
+        int age = randInRange(18, 30);
+        insertion(dp, id, age);
+        id += randOneOrTwo();
+    }
+
+    int queryAge = randInRange(18, 30);
+    searchAge(dp, queryAge);
+
+    int delAge = randInRange(18, 30);
+    deletion(dp, delAge);
+
+    maxAgeGap(dp);
+
+    destroyDataSet(dp);
+    return 0;
+}
